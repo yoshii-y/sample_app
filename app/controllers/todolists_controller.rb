5d5d09290507f421ee1024320996029bbfbe77a8
EC2 Default User <ec2-user@ip-172-31-15-67.ap-southeast-1.compute.internal>
2019-02-27 02:48:52 +0000

    [finish]

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,49 @@
+class TodolistsController < ApplicationController
+  def new
+    @list = List.new
+  end
+  
+  def create
+    # ストロングパラメーターを使用
+    list = List.new(list_params)
+    # DBへ保存する
+    list.save
+    # トップ画面へリダイレクト
+    redirect_to todolist_path(list.id)
+  end
+
+  def index
+    @lists=List.all
+  end
+  
+  def show
+    @list=List.find(params[:id])
+  end
+
+ def edit
+   @list=List.find(params[:id])
+ end
+
+ def update
+   list=List.find(params[:id])
+   list.update(list_params)
+   redirect_to todolist_path(list.id)
+ end
+
+  def destroy
+    list=List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
+  end
+  
+  
+  
+  
+  
+ 
+  private
+   
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'b4d285bd86a115259daf41c0c543910855214509f946df8d3c22513fee6e26e9131e47d2bb69ffd04781e0f47c89d55278a83d5257d7f53334f67d9f462abf1c'
